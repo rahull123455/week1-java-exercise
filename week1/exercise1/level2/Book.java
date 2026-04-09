94dca42449f603d2745a30ecb90b7412a4baa2a6
Vinayak kumar <125748758+rahull123455@users.noreply.github.com>
2026-04-10 04:18:46 +0530

    Refactor Book class by removing main method
    Removed main method and sample book initialization.

--- week1/exercise1/level2/Book.java
+++ week1/exercise1/level2/Book.java
@@ -1,20 +1,10 @@
 package week1.exercise1.level2;
 
 public class Book {
-
     String title;
     String author;
 
     public void displayInfo() {
         System.out.println("Title: " + title + ", Author: " + author);
-    }
-
-    public static void main(String[] args) {
-        Book myBook = new Book();
-
-        myBook.title = "The Great Gatsby";
-        myBook.author = "F. Scott Fitzgerald";
-
-        myBook.displayInfo();
     }
 }
